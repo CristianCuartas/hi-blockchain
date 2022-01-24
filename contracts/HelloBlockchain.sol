8c3f611a34645a0b093fe4bcf0f3a245cea3351d
Cristian Cuartas <cristianhz1109@gmail.com>
2022-01-24 10:58:41 -0500

    Add solidity file to smart contract and migration

---
+++ contracts/HelloBlockchain.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract HelloBlockchain {
+    function sayHi() public pure returns (string memory) {
+        return "Hi, blockchain!";
+    }
+}
